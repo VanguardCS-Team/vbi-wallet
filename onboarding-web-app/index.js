6b77e356e8b6cf50cc2426c06847069068a371d5
Frederick Pearson <fpearson@cloudstrucc.com>
2025-04-10 04:36:54 -0400

    adjusted readme.md

--- onboarding-web-app/index.js
+++ onboarding-web-app/index.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
 // ✅ Set up Handlebars as the template engine
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 // ✅ Serve static files
 app.use(express.static(path.join(__dirname, "public")));
