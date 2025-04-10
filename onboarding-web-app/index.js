c0795c6b212a74494cb557f3cb7f317bc5119952
Frederick Pearson <fpearson@cloudstrucc.com>
2025-04-10 04:31:41 -0400

    adjusted readme.md

--- onboarding-web-app/index.js
+++ onboarding-web-app/index.js
@@ -171,9 +171,5 @@ app.post("/logout", (req, res) => {
       res.redirect("/");
     });
   });
-  
-
-  
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
